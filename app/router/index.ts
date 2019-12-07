85f97a6e7038fe4cf2d9ba8ceeef3d188bf8a35b
lhmiao <2696254238@qq.com>
2019-12-07 16:54:10 +0800

    chore: 拆分路由

---
+++ app/router/index.ts
@@ -0,0 +1,16 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { Application } from 'egg';
+
+export type RouterFileModule = (app: Application) => void;
+
+export default (app: Application) => {
+  const routerFilesName = fs
+    .readdirSync(__dirname)
+    .filter(fileName => fileName !== 'index.ts');
+
+  routerFilesName.forEach(fileName => {
+    const filePath: string = path.join(__dirname, fileName);
+    (require(filePath).default as RouterFileModule)(app);
+  });
+};
