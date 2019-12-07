8819083004f38d00231dc0d032097b87d656d54a
lhmiao <2696254238@qq.com>
2019-12-07 16:49:01 +0800

    feat: 配置本地数据库连接

--- config/config.local.ts
+++ config/config.local.ts
@@ -2,5 +2,19 @@ import { EggAppConfig, PowerPartial } from 'egg';
 
 export default () => {
   const config: PowerPartial<EggAppConfig> = {};
+
+  config.sequelize = {
+    dialect: 'mysql',
+    database: 'book_share_platform',
+    host: '127.0.0.1',
+    username: 'root',
+    password: '123456',
+    define: {
+      freezeTableName: true,
+      timestamps: false,
+      underscored: false,
+    },
+  };
+
   return config;
 };
