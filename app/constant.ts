1978f58bef027ffd085c059d4b3dd02fbc19a7c8
lhmiao <2696254238@qq.com>
2019-12-08 21:24:03 +0800

    feat: add LOGIN_COOKIE_MAX_AGE

--- app/constant.ts
+++ app/constant.ts
@@ -1,3 +1,5 @@
+import ms from 'ms';
+
 export const SUCCESS_CODE: number = 0;
 
 export const ERROR_CODE: number = 1;
@@ -7,3 +9,5 @@ export const NEED_LOGIN_CODE: number = 2;
 export const NO_AUTH_CODE: number = 3;
 
 export const RES_BODY_KEYS: string[] = ['code', 'message', 'data'];
+
+export const LOGIN_COOKIE_MAX_AGE = ms('7d');
