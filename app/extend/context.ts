d5942ca131c05a205c7a5f9fc14dc3fd774c02ba
lhmiao <2696254238@qq.com>
2019-12-15 14:39:22 +0800

    chore: 添加注释

--- app/extend/context.ts
+++ app/extend/context.ts
@@ -1,6 +1,7 @@
 import SHA256 from 'crypto-js/sha256';
 import ms from 'ms';
 
+// 记录链节点
 interface ChainBlock {
   prevHash: string;
   timestamp: number;
@@ -8,6 +9,7 @@ interface ChainBlock {
   hash: string;
 }
 
+// 记录链类
 export class Chain {
   constructor(recordChain?: ChainBlock[]) {
     if (recordChain) {
@@ -42,13 +44,13 @@ export class Chain {
 }
 
 export const constant = {
-  SUCCESS_CODE: 0,
-  ERROR_CODE: 1,
-  NEED_LOGIN_CODE: 2,
-  NO_AUTH_CODE: 3,
-  RES_BODY_KEYS: ['code', 'message', 'data'],
-  LOGIN_COOKIE_MAX_AGE: ms('7d'),
-  CREATE_BOOK_CHANGE_NUMBER: 5,
-  CREATE_BOOK_COMMENT_CHANGE_NUMBER: 3,
-  CREATE_BOOK_COMMENT_ACTION_CHANGE_NUMBER: 1,
+  SUCCESS_CODE: 0, // 成功响应代码
+  ERROR_CODE: 1, // 失败响应代码
+  NEED_LOGIN_CODE: 2, // 未登录响应代码
+  NO_AUTH_CODE: 3, // 暂无权限响应代码
+  RES_BODY_KEYS: ['code', 'message', 'data'], // response body 结构的 key
+  LOGIN_COOKIE_MAX_AGE: ms('7d'), // 登录 cookie 有效期
+  CREATE_BOOK_BUSINESS_CHANGE_NUMBER: 5, // 完成交易奖励的图书币数额
+  CREATE_BOOK_COMMENT_CHANGE_NUMBER: 3, // 第一次评论某个图书奖励的图书币数额
+  CREATE_BOOK_COMMENT_ACTION_CHANGE_NUMBER: 1, // 评价某个评论奖励的图书币数额
 };
